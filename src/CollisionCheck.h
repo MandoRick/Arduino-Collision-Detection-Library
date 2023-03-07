3a5f07de9341b1bcac96084eff2ed769aa819d2f
Rick <66313067+MandoRick@users.noreply.github.com>
2023-03-07 04:14:37 +0000

    Update CollisionCheck.h

--- src/CollisionCheck.h
+++ src/CollisionCheck.h
@@ -12,7 +12,7 @@ public:
     //collisionDetection(obj1X, obj1Y, obj1W, obj1H, obj2X, obj2Y, obj2W, obj2H);
     bool collisionCheckSquareToSquare(int obj1X, int obj1Y, int obj1W, int obj1H, int obj2X, int obj2Y, int obj2W, int obj2H);
 
-    //collisionDetection(obj1X, obj1Y, obj1W, obj1H, obj2X, obj2Y, obj2W, obj2H);
+    //collisionDetection(obj1X, obj1Y, obj1R, obj2X, obj2Y, obj2R);
     bool collisionCheckRountToRound(int obj1X, int obj1Y, int obj1R, int obj2X, int obj2Y, int obj2R);
 
 private:
