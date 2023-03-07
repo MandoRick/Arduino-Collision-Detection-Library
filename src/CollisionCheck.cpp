f675333fea62469f2baee405721754d3d7fcdf11
Rick <66313067+MandoRick@users.noreply.github.com>
2023-03-07 04:13:32 +0000

    Add files via upload

---
+++ src/CollisionCheck.cpp
@@ -0,0 +1,19 @@
+#include "CollisionCheck.h"
+
+//collisionDetection(obj1X, obj1Y, obj1W, obj1H, obj2X, obj2Y, obj2W, obj2H);
+bool CollisionCheck::collisionCheckSquareToSquare(int obj1X, int obj1Y, int obj1W, int obj1H, int obj2X, int obj2Y, int obj2W, int obj2H) {
+  if ((obj1X + obj1W > obj2X && obj1X < obj2X + obj2W && obj1Y + obj1H > obj2Y && obj1Y < obj2Y + obj2H)
+      && (obj1X + obj1W > obj2X && obj1X < obj2X + obj2W && obj1Y + obj1H > obj2Y && obj1Y < obj2Y + obj2H)) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+bool CollisionCheck::collisionCheckRountToRound(int obj1X, int obj1Y, int obj1R, int obj2X, int obj2Y, int obj2R) {
+  if (((obj2X - obj1X) * (obj2X - obj1X)) + ((obj2Y - obj1Y) * (obj2Y - obj1Y)) < ((obj1R + obj2R) * (obj1R + obj2R))) {
+    return true;
+  } else {
+    return false;
+  }
+}
